7ff73c094631c777a4e0977c01fff1fc7edf1f4c
Josip <joske0000@gmai.com>
2024-12-05 20:01:54 +0100

    asd

--- obj/Debug/net8.0/Ispit.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/Ispit.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ispit.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3530c2bda1cc6a495e1948b92d628a2da153a8a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ispit.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ispit.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
